0efdbec968d6842d3d6d8a89a88f3cf374258ef2
Jveloper <soxo1027@gmail.com>
2019-07-11 20:26:11 +0900

    mongoose connect success

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
-// 1. express install
-// 2. nodemon
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const connectDB = require('./models/connection');
+
 
 const app = express();
 const port = 8080;
@@ -10,6 +11,8 @@ const port = 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('보내지나여 :3');
