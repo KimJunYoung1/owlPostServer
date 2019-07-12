1d49d26933906af07f08f0cf849593a944fcd65b
Jveloper <soxo1027@gmail.com>
2019-07-12 19:09:23 +0900

    jwt token create success

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const connectDB = require('./models/connection');
 
 const app = express();
+
 const port = 8080;
-const signUp = require('./middleware/signup');
+const signUp = require('./controllers/signup');
+const signin = require('./controllers/signin');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,11 +17,13 @@ app.use(cors());
 
 connectDB();
 
+
 app.get('/', (req, res) => {
   res.send('보내지나여 :3');
 });
 
 app.post('/user/signup', signUp);
+app.get('/user/signin', signin);
 
 app.listen(port, () => {
   console.log('sucess!');
---
+++ server/middleware/jwt.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken');
+const PRIVATE_KEY = require('../env');
+
+exports.jwtsign = email => {
+  console.log(PRIVATE_KEY);
+  const token = jwt.sign({ email }, PRIVATE_KEY, { expiresIn: '1m' });
+  return token;
+};
