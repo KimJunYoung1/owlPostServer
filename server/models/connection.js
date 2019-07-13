a151ea18f020d3c7e8b7b574e5744cccaf6eb145
Jveloper <soxo1027@gmail.com>
2019-07-13 15:18:05 +0900

    mongoDB PATH set up

--- server/models/connection.js
+++ server/models/connection.js
@@ -8,10 +8,11 @@ db.once('open', () => {
   console.log('connected to mongod server');
 });
 
-
 module.exports = () => {
   function connect() {
-    mongoose.connect(`mongodb://Jveloper:${process.env.PASSWORD}@52.15.57.160:27017/admin`, { useNewUrlParser: true });
+    mongoose.connect(`mongodb://${process.env.MONGO_URL}`, {
+      useNewUrlParser: true,
+    });
   }
   connect();
 };
