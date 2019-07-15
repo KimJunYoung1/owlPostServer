85d8ebfce133980109a43c9a12e8c34b186b8195
iee1020 <yl9536@gmail.com>
2019-07-15 20:58:06 +0900

    plus-email-nickname-overlap-check

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-const signUp = require('../controllers/signup');
+const overlapcheck = require('../controllers/signup/overlap-check');
+const signUp = require('../controllers/signup/signup');
+
 const signin = require('../controllers/signin');
+
 const sendletter = require('../controllers/sendletter');
 
+router.get('/signup/overlapcheck', overlapcheck);
 router.post('/signup', signUp);
 
 router.post('/letter-send', sendletter);
