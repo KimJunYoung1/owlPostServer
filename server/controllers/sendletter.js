d04753a3e4aa053a733a55b38ee0e8bfaa70b740
iee1020 <yl9536@gmail.com>
2019-07-15 13:45:00 +0900

    modify controllers sendletter

--- server/controllers/sendletter.js
+++ server/controllers/sendletter.js
@@ -1,19 +1,17 @@
 const LETTERS = require('../models/letters');
+const USERINFO = require('../models/userinfo');
 
 module.exports = (req, res) => {
-  const letter = new LETTERS();
-  letter.from = req.body.from;
-  letter.to = req.body.to;
-  letter.time = req.body.time;
-  letter.messages = req.body.messages;
-
-  letter.save(err => {
-    if (err) {
-      console.error(err);
-      res.json({ result: 0 });
-      return;
-    }
-
-    res.json({ result: 1 });
-  });
+  const { from, to } = req.body;
+  if (to !== null) {
+    USERINFO.findOne({ nickname: from, partner_nickName: to })
+      .then(() => {
+        LETTERS.collection.insertOne(req.body);
+        res.status(201).json({ result: 1 });
+      }).catch(err => {
+        res.status(400).json(err);
+      });
+  } else {
+    res.status(400).json('partner matching전 입니다.');
+  }
 };
