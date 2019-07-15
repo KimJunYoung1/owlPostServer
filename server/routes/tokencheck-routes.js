8654af828c41395da03b98ac73d145699d20131f
iee1020 <yl9536@gmail.com>
2019-07-15 22:57:19 +0900

    plus /check/cutmatch

--- server/routes/tokencheck-routes.js
+++ server/routes/tokencheck-routes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const mypage = require('../controllers/mypage');
 const postbox = require('../controllers/postbox');
+const cutmatch = require('../controllers/partner-cutmatch');
 
 router.get('/mypage', mypage);
 router.get('/postbox', postbox);
+router.put('/cutmatch', cutmatch);
 
 module.exports = router;
