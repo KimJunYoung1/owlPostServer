15c77e8c2a722f1b9780e451e29080ba8d16ae5b
iee1020 <yl9536@gmail.com>
2019-07-15 16:06:40 +0900

    controllers/signup-modify-res.status(201).json()

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
       .then(result => {
         if (result === null) {
           USERINFO.collection.insertOne(req.body);
-          res.status(201);
+          res.status(201).json();
         } else {
           res.status(400).json('이미 등록된 계정입니다.');
         }
