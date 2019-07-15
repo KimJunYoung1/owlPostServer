8332b61cce636c0136a131964fb8b2f02c41dfab
iee1020 <yl9536@gmail.com>
2019-07-15 13:44:20 +0900

    modify middleware jwt.sign function

--- server/middleware/jwt.js
+++ server/middleware/jwt.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 exports.jwtsign = email => {
   // process.env.secret
-  console.log(process.env.PRIVATE_KEY);
+  // console.log(process.env.PRIVATE_KEY);
   const token = jwt.sign({ email }, `${process.env.PRIVATE_KEY}`, { expiresIn: '1m' });
   return token;
 };
