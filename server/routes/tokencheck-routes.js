930ffd7605b0145030f6bbece45d103e6e2608da
Jveloper <soxo1027@gmail.com>
2019-07-15 22:30:23 +0900

    postbox route function

--- server/routes/tokencheck-routes.js
+++ server/routes/tokencheck-routes.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const mypage = require('../controllers/mypage');
-
+const postbox = require('../controllers/postbox');
 
 router.get('/mypage', mypage);
+router.get('/postbox', postbox);
 
 module.exports = router;
