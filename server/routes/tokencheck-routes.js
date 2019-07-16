6bfa3a36788fb45a82c78e4262eebb082025ee32
iee1020 <yl9536@gmail.com>
2019-07-16 20:57:08 +0900

    modify tokencheck-routes.js

--- server/routes/tokencheck-routes.js
+++ server/routes/tokencheck-routes.js
@@ -9,9 +9,11 @@ const match = require('../controllers/partner/partner-match');
 const blacklist = require('../controllers/blacklist');
 const withdrawal = require('../controllers/delete/withdrawal');
 const deleteletter = require('../controllers/delete/delete-letters');
+const home = require('../controllers/homesenddata');
 
 router.get('/mypage', mypage);
 router.get('/postbox', postbox);
+router.get('/home', home);
 
 router.post('/match', match);
 router.post('/blacklist', blacklist);
