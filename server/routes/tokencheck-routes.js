9a620f5ba00e5bb2e91999be22ba3e4047053083
iee1020 <yl9536@gmail.com>
2019-07-16 15:06:39 +0900

    withdrawal.js plus in check-routes.js

--- server/routes/tokencheck-routes.js
+++ server/routes/tokencheck-routes.js
@@ -4,14 +4,19 @@ const router = express.Router();
 
 const mypage = require('../controllers/mypage');
 const postbox = require('../controllers/postbox');
-const cutmatch = require('../controllers/partner-cutmatch');
-const match = require('../controllers/partner-match');
+const cutmatch = require('../controllers/partner/partner-cutmatch');
+const match = require('../controllers/partner/partner-match');
 const blacklist = require('../controllers/blacklist');
+const withdrawal = require('../controllers/delete/withdrawal');
 
 router.get('/mypage', mypage);
 router.get('/postbox', postbox);
-router.put('/cutmatch', cutmatch);
+
 router.post('/match', match);
 router.post('/blacklist', blacklist);
+
+router.put('/cutmatch', cutmatch);
+
+router.delete('/withdrawal', withdrawal);
 
 module.exports = router;
