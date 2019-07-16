87b783f76b086f4e08e7615132c0cac694f23a1b
iee1020 <yl9536@gmail.com>
2019-07-16 20:44:51 +0900

    modify overlap-check

--- server/controllers/signup/overlap-check.js
+++ server/controllers/signup/overlap-check.js
@@ -3,17 +3,15 @@ const USERINFO = require('../../models/userinfo');
 // email, nickname  중복 체크
 module.exports = (req, res) => {
   const reqKey = Object.keys(req.query)[0];
-  const obj = {};
-  obj[reqKey] = req.query[reqKey];
-
-  USERINFO.findOne(obj)
+  USERINFO.findOne(req.query)
     .then(result => {
       if (result !== null) {
         res.status(400).json(`중복된 ${reqKey}입니다.`);
       } else if (result === null) {
         res.status(200).json(`사용 가능한 ${reqKey}입니다.`);
       }
-    }).catch(err => {
+    })
+    .catch(err => {
       res.status(400).json(err);
     });
 };
