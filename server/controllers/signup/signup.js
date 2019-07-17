178e48f453d850fb1dff902a3f079ad421e15bff
Jveloper <soxo1027@gmail.com>
2019-07-17 20:57:41 +0900

    sign in, sign up hashing complete

--- server/controllers/signup/signup.js
+++ server/controllers/signup/signup.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const USERINFO = require('../../models/userinfo');
 
 module.exports = (req, res) => {
@@ -7,7 +8,28 @@ module.exports = (req, res) => {
     USERINFO.findOne({ email })
       .then(result => {
         if (result === null) {
-          USERINFO.collection.insertOne(req.body);
+          const shasum = crypto
+            .createHash('sha512')
+            .update(`${req.body.password}i love coffee`)
+            .digest('base64');
+
+          // crypto.randomBytes(64, (err, buf) => {
+          //   crypto.pbkdf2(shasum, buf.toString('base64'), 126587, 64, 'sha512', (_err, key) => {
+          //     console.log(key.toString('base64'));
+          //   });
+          // });
+
+
+          USERINFO.collection.insertOne({
+            email: req.body.email,
+            password: shasum,
+            nickname: req.body.nickname,
+            age: req.body.age,
+            sex: req.body.sex,
+            select: req.body.select,
+            blackList: req.body.blackList,
+            partner_nickname: req.body.partner_nickname,
+          });
           res.status(201).json('회원가입 되었습니다.');
         } else {
           res.status(400).json('이미 등록된 계정입니다.');
