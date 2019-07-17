997de264b78e2282d311fd44d77d2b099fffce67
iee1020 <yl9536@gmail.com>
2019-07-17 20:21:15 +0900

    partner-cutmatch modify partner.partner_nickname to be null

--- server/controllers/partner/partner-cutmatch.js
+++ server/controllers/partner/partner-cutmatch.js
@@ -1,20 +1,24 @@
 const USERINFO = require('../../models/userinfo');
 
-module.exports = (req, res) => {
-  USERINFO.findOne({ email: req.decode })
-    .then(result => {
-      if (result.partner_nickname === null) {
-        res.status(403).json('matching된 partner가 없습니다.');
-      } else if (result.partner_nickname !== null) {
-        USERINFO.update(result, { partner_nickname: null }, (error, update) => {
-          if (error) {
-            res.status(400).json(error);
-          }
-          res.status(201).json(`partner ${update}와의 관계가 성공적으로 끊겼습니다.`);
-        });
-      }
-    })
-    .catch(error => {
-      res.status(404).json(error);
-    });
+module.exports = async (req, res) => {
+  try {
+    const userInfo = await USERINFO.findOne({ email: req.decode });
+    if (userInfo.partner_nickname === null) {
+      res.status(403).json('matching된 partner가 없습니다.');
+    } else if (userInfo.partner_nickname !== null) {
+      const partnerInfo = await USERINFO.findOne({ nickname: userInfo.partner_nickname });
+      const updateRe = async data => {
+        try {
+          await USERINFO.updateOne(data, { partner_nickname: null });
+          res.status(201).json('partner의 관계가 성공적으로 끊겼습니다.');
+        } catch (error) {
+          res.status(400).json(`cutmatcherror: ${error}`);
+        }
+      };
+      updateRe(partnerInfo);
+      updateRe(userInfo);
+    }
+  } catch (error) {
+    res.status(400).json(`cutmatchfinderror: ${error}`);
+  }
 };
