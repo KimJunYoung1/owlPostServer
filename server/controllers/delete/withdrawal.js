d8dd2b7902ac76bb900a5c08259c51aae3ba80ad
iee1020 <yl9536@gmail.com>
2019-07-17 20:21:57 +0900

    withdrawal modify partner.partner_nickname to be null

--- server/controllers/delete/withdrawal.js
+++ server/controllers/delete/withdrawal.js
@@ -1,21 +1,18 @@
 const USERINFO = require('../../models/userinfo');
 
-module.exports = (req, res) => {
-  USERINFO.findOne({ email: req.decode })
-    .then(result => {
-      if (result === null) {
-        res.status(403).json('존재하지 않는 유저 정보입니다.');
-      } else {
-        USERINFO.deleteOne(result)
-          .then(() => {
-            res.status(200).json('성공적으로 회원 탈퇴 되었습니다.');
-          })
-          .catch(error => {
-            res.status(400).json(`deleteError: ${error}`);
-          });
-      }
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+module.exports = async (req, res) => {
+  try {
+    const userInfos = await USERINFO.findOne({ email: req.decode });
+    const partnerInfos = await USERINFO.findOne({ nickname: userInfos.partner_nickname });
+
+    if (userInfos === null) {
+      res.status(403).json('존재하지 않는 유저 정보입니다.');
+    } else {
+      await USERINFO.updateOne({ email: partnerInfos.email }, { partner_nickname: null });
+      await USERINFO.deleteOne(userInfos);
+      res.status(200).json('성공적으로 회원 탈퇴 되었습니다.');
+    }
+  } catch (error) {
+    res.status(400).json(`withdrawalError: ${error}`);
+  }
 };
