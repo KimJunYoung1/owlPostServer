f9d98ab029e1d0fd1aedfa943de381e494da3e98
Jveloper <soxo1027@gmail.com>
2019-07-19 12:59:16 +0900

    matching modify

--- server/controllers/partner/partner-match.js
+++ server/controllers/partner/partner-match.js
@@ -4,89 +4,111 @@ const USERINFO = require('../../models/userinfo');
 module.exports = async (req, res) => {
   const { nickname } = req.query;
   const user = await USERINFO.findOne({ nickname });
+  const userinfos = [user.nickname, user.age, user.sex, user.select, user.partner_nickname, user.blackList];
+  // 현재 사용하고 있는 유저 정보
   
-  const userinfos = [user.nickname, user.sex, user.select, user.partner_nickname, user.blackList];
-  
-  const results = await USERINFO.find({}, (err, docs) => {
-    // await을 위해서 어쩔수 없이 넣어줌
+  const results = await USERINFO.find({}, (err, docs) => {return docs});
+  // 모든 유저들의 정보
     
-    if (req.decode === user.email) {    
-      const partnerExist = docs.map(users => {
-        if (users.partner_nickname === null) { return [users.nickname, users.sex, users.select, users.partner_nickname, users.blackList]; }
-      });
+  const partnerNullUserInfos = results.map(partnerInfos => {
+    if (partnerInfos.partner_nickname === null) { return [partnerInfos.nickname, partnerInfos.age, partnerInfos.sex, partnerInfos.select, partnerInfos.partner_nickname, partnerInfos.blackList]; }
+  });
+  // 파트너 닉네임이 null인 모든 유저들의 정보  
+  
+  const copyRandomSelectUser = [];
 
-      const recur = function () {
-        const randomSelectUser = partnerExist[Math.floor(Math.random() * partnerExist.length - 1)];
-        // 0번째인자 닉네임, 1번째인자 남자/여자, 2번째인자 이성/상관없음, 3번째인자 파트너닉네임
+      const recur = function () {        
+        const randomSelectUser = partnerNullUserInfos[Math.floor(Math.random() * partnerNullUserInfos.length - 1)];
+        copyRandomSelectUser.push(randomSelectUser);
+        // 0번째인자 닉네임, 1번째인자 남자/여자, 2번째인자 이성/상관없음, 3번째인자 파트너닉네임, 4번째인자 블랙리스트
 
-
-        const match = function(userNickName, partnerNickName) {
-          for(let i = 0; i < userinfos[4].length; i++){
-            if(userinfos[4][i] === partnerNickName){
-              return recur();
+          const match = function(userNickName, partnerNickName) {
+            for(let i = 0; i < userinfos[5].length; i++){
+              if(userinfos[5][i] === partnerNickName){
+                return recur();
+              }
             }
-          }
-          for(let j = 0; j < randomSelectUser[4].length; j++){
-            if(randomSelectUser[4][j] === userNickName){
-              return recur();
+            // 나의 블랙리스트에 상대방이 없는지 확인
+            for(let j = 0; j < randomSelectUser[5].length; j++){
+              if(randomSelectUser[5][j] === userNickName){
+                return recur();
+              }
             }
-          }
-          const partnerNick = partnerNickName;
-          const userNick = userNickName;          
-          userinfos[3] = partnerNick;
-          randomSelectUser[3] = userNick;
-          (async () => {
-            await USERINFO.findOne({ nickname: userNick })
-            await USERINFO.updateOne({ nickname: userNick }, { partner_nickname: partnerNick })
-            await USERINFO.findOne({ nickname: partnerNick })
-            await USERINFO.updateOne({ nickname: partnerNick}, { partner_nickname: userNick })
-          })();          
-        };
-        // 아래 조건문에 따라 매칭을 시켜줄 함수
+            // 상대방의 블랙리스트에 내가 없는지 확인
+            if(userinfos[0] === randomSelectUser[0]){
+              return ;
+            }
+            // 나의 닉네임과 상대방의 닉네임이 같다면 return and 401 error
+            if(userinfos[1] !== randomSelectUser[1]){
+              return;
+            }
+            // 나의 나이와 상대방의 나이가 다르다면 return and 400 error
+            
+            // const partnerNick = partnerNickName;
+            // const userNick = userNickName;          
+            // userinfos[4] = partnerNick;
+            // randomSelectUser[4] = userNick;
+            (async () => {
+              const me = await USERINFO.findOne({ nickname: userNickName })
+              
+              await USERINFO.updateOne({ nickname: me.nickname }, { partner_nickname: me.partner_nickname })
+              const you = await USERINFO.findOne({ nickname: partnerNickName })
+              
+              await USERINFO.updateOne({ nickname: you.nickname}, { partner_nickname: you.partner_nickname })
+            })();                      
+          };
+          // 아래 조건문에 따라 매칭을 시켜줄 함수
 
-        if (randomSelectUser !== undefined) {
-          if (userinfos[2] === false && randomSelectUser[2] === false) {
+        if (randomSelectUser !== undefined && userinfos[0] !== randomSelectUser[0]) {
+          if (userinfos[3] === false && randomSelectUser[3] === false) {
             // 둘다 상관없을때 매칭
 
-            match(userinfos[0], randomSelectUser[0]);
-          } else if (userinfos[2] === true && userinfos[1] === true && randomSelectUser[1] === false) {
+            return match(userinfos[0], randomSelectUser[0]);
+          } else if (userinfos[3] === true && userinfos[2] === true && randomSelectUser[2] === false) {
             // 남자인데 이성만 원할때 여자
 
-            match(userinfos[0], randomSelectUser[0]);
-          } else if (userinfos[1] === false && randomSelectUser[2] === true && randomSelectUser[1] === true) {
+            return match(userinfos[0], randomSelectUser[0]);
+          } else if (userinfos[2] === false && randomSelectUser[3] === true && randomSelectUser[2] === true) {
             // 여자인데 상대방이 이성만 원하고 남자일때
 
-            match(userinfos[0], randomSelectUser[0]);
-          } else if (userinfos[2] === true && userinfos[1] === false && randomSelectUser[1] === true) {
+            return match(userinfos[0], randomSelectUser[0]);
+          } else if (userinfos[3] === true && userinfos[2] === false && randomSelectUser[2] === true) {
             // 여자인데 이성만 원할때 남자
 
-            match(userinfos[0], randomSelectUser[0]);
-          } else if (userinfos[1] === true && randomSelectUser[2] === true && randomSelectUser[1] === false) {
+            return match(userinfos[0], randomSelectUser[0]);
+          } else if (userinfos[2] === true && randomSelectUser[3] === true && randomSelectUser[2] === false) {
             // 남자인데 상대방이 이성만 원하고 여자일때
 
-            match(userinfos[0], randomSelectUser[0]);
-          } else if (userinfos[1] === true && userinfos[2] === true && randomSelectUser[1] === false && randomSelectUser[2] && true) {
+            return match(userinfos[0], randomSelectUser[0]);
+          } else if (userinfos[2] === true && userinfos[3] === true && randomSelectUser[2] === false && randomSelectUser[3] && true) {
             // 남자인데 이성만 원하고 여자인데 이성만 원할때
 
-            match(userinfos[0], randomSelectUser[0]);
+            return match(userinfos[0], randomSelectUser[0]);
+          } else {
+            return ;
           }
-        }       
+        }           
       };
-      recur();
-      
-    //   process.exit();
-    } else {
-      res.status(400).json('재로그인이 필요합니다')
-    }
-  });
+      recur();    
   
-  if (userinfos[3] !== null) {
-    console.log('who your partner', userinfos);
-    res.status(200).json('매칭이 완료되었습니다');
-  } else if(userinfos[3] === userinfos[0]){
-    res.status(401).json('현재 매칭가능한 상대가 없습니다')
+  if(copyRandomSelectUser.length > 0){
+    if (userinfos[4] !== null) {      
+      res.status(200).json('매칭이 완료되었습니다');
+      return;      
+    } else if(userinfos[1] !== copyRandomSelectUser[copyRandomSelectUser.length - 1][1]){
+      res.status(400).json('매칭이 진행중입니다');
+      return;
+    } else if(userinfos[0] === copyRandomSelectUser[copyRandomSelectUser.length - 1][0]){
+      res.status(401).json('매칭이 진행중입니다');
+      return;
+    } else {
+      res.status(402).json('매칭이 진행중입니다');
+      return;
+    }
   } else {
-    res.status(400).json('매칭이 진행중입니다');
+    res.status(403).json('매칭이 진행중입니다');
+    return;
   }
+  
 };
 // 비동기처리로 인하여 한번씩 sync가 안맞아서 내려가면서 읽는순서가 뒤틀릴때가 존재함, fetch 를 5분에 한번정도씩 요청해서 처리해야 할 듯
